78e9d113998a8cdacb8c2b588a3cf8072fd5577e
rasme14 <compchap.megha@gmail.com>
2015-12-25 15:53:05 -0600

    Whole Project

---
+++ iRemember/app/build/generated/source/buildConfig/debug/edu/vanderbilt/mooc/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package edu.vanderbilt.mooc;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "edu.vanderbilt.mooc";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 10;
+  public static final String VERSION_NAME = "0.1.0-SNAPSHOT";
+}
